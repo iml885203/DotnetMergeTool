0576bb6bab3c5dfd6bc1788078a70afd887763e7
Logan <iml885203@gmail.com>
2024-05-05 00:52:59 +0800

    🔨 update script

--- local-build-install.sh
+++ local-build-install.sh
@@ -1,4 +1,4 @@
-dotnet build MergeTool/MergeTool.csproj
-dotnet publish MergeTool/MergeTool.csproj -r osx-x64 --self-contained -o "publish"
+dotnet build MergeTool/MergeTool.csproj --configuration Release --no-restore
+dotnet publish MergeTool/MergeTool.csproj -c Release -r osx-x64 --self-contained -o "publish"
 sudo mv publish/MergeTool /usr/local/bin
 rm -r publish
